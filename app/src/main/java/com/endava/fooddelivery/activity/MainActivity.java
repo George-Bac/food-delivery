2bbd70e8b490dd3da39a497cac80bc1011cdcd0b
George-Bac <george.bacalu@endava.com>
2022-10-06 08:49:46 +0300

    Finish declaring the view holder item of the popular food recycler view adapter and make the code in the MainActivity more reusable (finish MainActivity)

--- app/src/main/java/com/endava/fooddelivery/activity/MainActivity.java
+++ app/src/main/java/com/endava/fooddelivery/activity/MainActivity.java
@@ -24,27 +24,30 @@ public class MainActivity extends AppCompatActivity {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
 
+      initViews();
+      configureRecyclerViewAdapter(recyclerViewCategories, categoryRecyclerViewAdapter);
+      configureRecyclerViewAdapter(recyclerViewFood, foodRecyclerViewAdapter);
+   }
+
+   private void initViews() {
       recyclerViewCategories = findViewById(R.id.recyclerViewCategories);
       recyclerViewFood = findViewById(R.id.recyclerViewFood);
-
-      List<Category> categories = List.of(
+      categoryRecyclerViewAdapter = new CategoryRecyclerViewAdapter(List.of(
             new Category("Pizza", "cat_1"),
             new Category("Burger", "cat_2"),
             new Category("Hotdog", "cat_3"),
             new Category("Drink", "cat_4"),
             new Category("Donut", "cat_5")
-      );
-      categoryRecyclerViewAdapter = new CategoryRecyclerViewAdapter(categories);
-      recyclerViewCategories.setAdapter(categoryRecyclerViewAdapter);
-      recyclerViewCategories.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+      ));
+      foodRecyclerViewAdapter = new FoodRecyclerViewAdapter(List.of(
+            new Food("Pepperoni pizza", "pop_1", "slices pepperoni, mozzarella cheese, fresh oregano, ground black pepper, sauce", 9.76),
+            new Food("Cheese Burger", "pop_2", "beef, gouda cheese, special sauce, lettuce, tomatoes", 8.79),
+            new Food("Vegetable pizza", "pop_3", "olive oil, vegetable oil, pitted kalamata, cherry tomatoes, fresh oregano, basil", 8.50)
+      ));
+   }
 
-      List<Food> foods = List.of(
-            new Food("Pepperoni pizza", "pizza1", "slices pepperoni, mozarella cheese, fresh oregano, ground black pepper, sauce", 9.76),
-            new Food("Cheese Burger", "burger", "beef, gouda cheese, special sauce, lettuce, tomatoes", 8.79),
-            new Food("Vegetable pizza", "pizza2", "olive oil, vegetable oil, pitted kalamata, cherry tomatoes, fresh oregano, basil", 8.50)
-      );
-      foodRecyclerViewAdapter = new FoodRecyclerViewAdapter(foods);
-      recyclerViewFood.setAdapter(foodRecyclerViewAdapter);
-      recyclerViewFood.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+   private void configureRecyclerViewAdapter(RecyclerView recyclerView, RecyclerView.Adapter recyclerViewAdapter) {
+      recyclerView.setAdapter(recyclerViewAdapter);
+      recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
    }
 }
--- app/src/main/java/com/endava/fooddelivery/adapter/FoodRecyclerViewAdapter.java
+++ app/src/main/java/com/endava/fooddelivery/adapter/FoodRecyclerViewAdapter.java
@@ -21,61 +21,47 @@ import java.util.List;
 
 public class FoodRecyclerViewAdapter extends RecyclerView.Adapter<FoodRecyclerViewAdapter.ViewHolder> {
 
-   private List<Food> foods = new ArrayList<>();
-   private String picUrl;
+   private List<Food> popularFood = new ArrayList<>();
 
-   public FoodRecyclerViewAdapter(List<Food> foods) {
-      this.foods = foods;
+   public FoodRecyclerViewAdapter(List<Food> popularFood) {
+      this.popularFood = popularFood;
    }
 
    @NonNull
    @Override
    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-      return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_category, parent, false));
+      return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_popular_food, parent, false));
    }
 
    @Override
    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
-      viewHolder.categoryTitle.setText(foods.get(position).getTitle());
-      viewHolder.categoryTitle.setText(foods.get(position).getTitle());
-      picUrl = "";
-      switch (position) {
-         case 0: setPicUrlAndBackground(viewHolder, "cat_1", R.drawable.category_background1); break;
-         case 1: setPicUrlAndBackground(viewHolder, "cat_2", R.drawable.category_background2); break;
-         case 2: setPicUrlAndBackground(viewHolder, "cat_3", R.drawable.category_background3); break;
-         case 3: setPicUrlAndBackground(viewHolder, "cat_4", R.drawable.category_background4); break;
-         case 4: setPicUrlAndBackground(viewHolder, "cat_5", R.drawable.category_background5); break;
-      }
-      int drawableResourceId = viewHolder.itemView.getContext().getResources().getIdentifier(picUrl, "drawable", viewHolder.itemView.getContext().getPackageName());
-      Glide.with(viewHolder.itemView.getContext()).load(drawableResourceId).into(viewHolder.categoryPic);
-   }
-
-   private void setPicUrlAndBackground(@NonNull ViewHolder viewHolder, String newPicUrl, int categoryBackground) {
-      picUrl = newPicUrl;
-      viewHolder.mainLayout.setBackground(ContextCompat.getDrawable(viewHolder.itemView.getContext(), categoryBackground));
+      viewHolder.itemTitle.setText(popularFood.get(position).getTitle());
+      viewHolder.itemFee.setText(String.valueOf(popularFood.get(position).getFee()));
+      int drawableResourceId = viewHolder.itemView.getContext().getResources().getIdentifier(popularFood.get(position).getPic(), "drawable", viewHolder.itemView.getContext().getPackageName());
+      Glide.with(viewHolder.itemView.getContext()).load(drawableResourceId).into(viewHolder.itemPic);
    }
 
    @Override
    public int getItemCount() {
-      return foods.size();
+      return popularFood.size();
    }
 
-   public void setFoods(List<Food> foods) {
-      this.foods = foods;
+   public void setPopularFood(List<Food> popularFood) {
+      this.popularFood = popularFood;
       notifyDataSetChanged();
    }
 
    public class ViewHolder extends RecyclerView.ViewHolder {
-      private TextView categoryTitle;
-      private ImageView categoryPic;
-      private ConstraintLayout mainLayout;
+      private TextView itemTitle, itemFee, itemAddButton;
+      private ImageView itemPic;
 
       public ViewHolder(@NonNull View itemView) {
          super(itemView);
 
-         categoryTitle = itemView.findViewById(R.id.categoryTitle);
-         categoryPic = itemView.findViewById(R.id.categoryPic);
-         mainLayout = itemView.findViewById(R.id.mainLayout);
+         itemTitle = itemView.findViewById(R.id.itemTitle);
+         itemFee = itemView.findViewById(R.id.itemFee);
+         itemPic = itemView.findViewById(R.id.itemPic);
+         itemAddButton = itemView.findViewById(R.id.itemAddButton);
       }
    }
 }
