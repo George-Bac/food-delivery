4e07ddc2b15395970485d19aa228a4b57f5f5604
George-Bac <george.bacalu@endava.com>
2022-10-20 20:27:55 +0300

    Use lombok to reduce boiler plate code in the model classes

--- app/src/main/java/com/endava/fooddelivery/model/Category.java
+++ app/src/main/java/com/endava/fooddelivery/model/Category.java
@@ -1,51 +1,21 @@
 package com.endava.fooddelivery.model;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Category {
-   
+
    private String title;
    private String pic;
 
    public Category(String title, String pic) {
       this.title = title;
       this.pic = pic;
-   }
-
-   public String getTitle() {
-      return title;
-   }
-
-   public void setTitle(String title) {
-      this.title = title;
-   }
-
-   public String getpic() {
-      return pic;
-   }
-
-   public void setpic(String pic) {
-      this.pic = pic;
-   }
-
-   @Override
-   public String toString() {
-      return "Category{" +
-            "title='" + title + '\'' +
-            ", pic='" + pic + '\'' +
-            '}';
-   }
-
-   @Override
-   public boolean equals(Object o) {
-      if (this == o) return true;
-      if (o == null || getClass() != o.getClass()) return false;
-      Category category = (Category) o;
-      return title.equals(category.title) && pic.equals(category.pic);
-   }
-
-   @Override
-   public int hashCode() {
-      return Objects.hash(title, pic);
    }
 }
