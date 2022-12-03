9d40ec9d6a1e763c8c6454b429738d0573dfef4d
George-Bac <george.bacalu@endava.com>
2022-12-03 15:43:28 +0200

    Remove lombok annotation from model classes

--- app/src/main/java/com/endava/fooddelivery/model/Category.java
+++ app/src/main/java/com/endava/fooddelivery/model/Category.java
@@ -1,14 +1,9 @@
 package com.endava.fooddelivery.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import androidx.annotation.NonNull;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
+import java.util.Objects;
+
 public class Category {
 
    private String title;
@@ -17,5 +12,43 @@ public class Category {
    public Category(String title, String pic) {
       this.title = title;
       this.pic = pic;
+   }
+
+   public String getTitle() {
+      return title;
+   }
+
+   public void setTitle(String title) {
+      this.title = title;
+   }
+
+   public String getPic() {
+      return pic;
+   }
+
+   public void setPic(String pic) {
+      this.pic = pic;
+   }
+
+   @NonNull
+   @Override
+   public String toString() {
+      return "Category{" +
+            "title='" + title + '\'' +
+            ", pic='" + pic + '\'' +
+            '}';
+   }
+
+   @Override
+   public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      Category category = (Category) o;
+      return title.equals(category.title) && pic.equals(category.pic);
+   }
+
+   @Override
+   public int hashCode() {
+      return Objects.hash(title, pic);
    }
 }
--- app/src/main/java/com/endava/fooddelivery/model/Food.java
+++ app/src/main/java/com/endava/fooddelivery/model/Food.java
@@ -1,16 +1,10 @@
 package com.endava.fooddelivery.model;
 
+import androidx.annotation.NonNull;
+
 import java.io.Serializable;
+import java.util.Objects;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
 public class Food implements Serializable {
 
    private String title;
@@ -24,5 +18,74 @@ public class Food implements Serializable {
       this.pic = pic;
       this.description = description;
       this.fee = fee;
+   }
+
+   public String getTitle() {
+      return title;
+   }
+
+   public void setTitle(String title) {
+      this.title = title;
+   }
+
+   public String getPic() {
+      return pic;
+   }
+
+   public void setPic(String pic) {
+      this.pic = pic;
+   }
+
+   public String getDescription() {
+      return description;
+   }
+
+   public void setDescription(String description) {
+      this.description = description;
+   }
+
+   public Double getFee() {
+      return fee;
+   }
+
+   public void setFee(Double fee) {
+      this.fee = fee;
+   }
+
+   public int getNumberInCart() {
+      return numberInCart;
+   }
+
+   public void setNumberInCart(int numberInCart) {
+      this.numberInCart = numberInCart;
+   }
+
+   @NonNull
+   @Override
+   public String toString() {
+      return "Food{" +
+            "title='" + title + '\'' +
+            ", pic='" + pic + '\'' +
+            ", description='" + description + '\'' +
+            ", fee=" + fee +
+            ", numberInCart=" + numberInCart +
+            '}';
+   }
+
+   @Override
+   public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      Food food = (Food) o;
+      return numberInCart == food.numberInCart &&
+            title.equals(food.title) &&
+            pic.equals(food.pic) &&
+            description.equals(food.description) &&
+            fee.equals(food.fee);
+   }
+
+   @Override
+   public int hashCode() {
+      return Objects.hash(title, pic, description, fee, numberInCart);
    }
 }
